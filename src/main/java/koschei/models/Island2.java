e7fef98f8ccb9bfe42bd00c2298c0cf6a2164600
Fias7777 <102873032+Fias7777@users.noreply.github.com>
2023-09-08 14:48:30 +0300

    add file gitignore, change @Bean in AppConfig

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -3,13 +3,21 @@ package koschei.models;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Island2 {
 
     private Wood3 wood;
 
     @Autowired
     public Island2(Wood3 wood) {
+        this.wood = wood;
+    }
+
+    public Wood3 getWood() {
+        return wood;
+    }
+
+    public void setWood(Wood3 wood) {
         this.wood = wood;
     }
 
--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 public class Ocean1 {
 
     @Autowired
-    @Qualifier("island2")
     private Island2 island;
 
     @Override
